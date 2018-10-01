8cbca0a98ec8da7fa5a5cbdac09c9b0a8f12fedc
mariakatusiime <mariakatusiime@gmail.com>
2018-10-01 16:22:44 +0300

    [ch]Set up database and created tables in it [Deliver #160870038]

--- controllers/db.py
+++ controllers/db.py
@@ -6,8 +6,10 @@ curs = con.cursor()
 curs.execute('''CREATE TABLE ORDERS(
              ORDER_NO INT PRIMARY KEY    NOT NULL,
              DISH VARCHAR(320)     NOT NULL,
-             PRICE   FLOAT(6)     NOT NULL,
-             STATUS  
+             PRICE   FLOAT(6)      NOT NULL,
+             STATUS  TEXT
+             
+
              ); ''')
 print("Table created successfully")
 #con.commit()
@@ -22,23 +24,20 @@ print("Table created successfully")
 #curs.execute("INSERT INTO USERS(ID,USERNAME,EMAIL,PASSWORD)\
 #             VALUES(4,'jacob','jcob@hotmail.com','green');")
 #con.commit()
-curs.execute("INSERT INTO MENU(ID,DISH,PRICE)\
-             VALUES(1,'Fried fish ',1200);")
-curs.execute("INSERT INTO MENU(ID,DISH,PRICE)\
-             VALUES(2,'Fish in G.NUTS',1800);")
-curs.execute("INSERT INTO MENU(ID,DISH,PRICE)\
-             VALUES(3,'Beef',1500);")
+curs.execute("INSERT INTO ORDERS(ORDER_NO,DISH,PRICE,STATUS)\
+             VALUES(1,'Fried fish ',1200,'ACCEPTED');")
 
 #curs.execute("UPDATE USERS SET USERNAME ='kevin' WHERE ID = 1 ")
 con.commit()
 #curs.execute("DELETE FROM USERS WHERE USERNAME = 'kevin'")
 #con.commit()
-curs.execute("SELECT ID,DISH,PRICE FROM MENU")
+curs.execute("SELECT ORDER_NO,DISH,PRICE,STATUS FROM ORDERS")
 dishlist = curs.fetchall()
 for dish in dishlist:
-    print("ID = ",dish[0])
+    print("ORDER_NO = ",dish[0])
     print("DISH = ",dish[1])
-    print("PRICE = ",dish[2],"\n")
+    print("PRICE = ",dish[2])
+    print("STATUA = ",dish[3],"\n")
 print("Operation succesful")
 #con.commit()
 con.close()
