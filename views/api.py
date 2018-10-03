a3ea725d7100ac6d2806c6e1082a324558de8fd3
mariakatusiime <mariakatusiime@gmail.com>
2018-10-03 18:23:27 +0300

    [ft] Admin can add menu items[Deliver #160870202]

--- views/api.py
+++ views/api.py
@@ -2,6 +2,7 @@ from flask import Flask,jsonify,abort,make_response,request
 from controllers.db import Dbase
 from models.ordermodel import CustomerOrders
 from models.usermodel import Users
+from models.menumodel import Menu
 
 myapp = Flask(__name__)
 
@@ -13,7 +14,7 @@ def index():
     return jsonify({'Hoody':'Welcome to my web app'}),200
 
 
-@myapp.route("/auth/signup/",methods=['POST'])
+@myapp.route("/api/v2/auth/signup/",methods=['POST'])
 def user_signup():
     log = request.json
     details = Users(log.get('username'),log.get('email'),log.get('password'))
@@ -22,7 +23,7 @@ def user_signup():
 
 
 
-@myapp.route("/auth/login/",methods=['POST'])
+@myapp.route("/api/v2/auth/login/",methods=['POST'])
 def user_login():
     log = request.json
     username =log.get('username')
@@ -35,44 +36,54 @@ def user_login():
 
 
 
-@myapp.route("/users/orders/",methods=['GET'])
+@myapp.route("/api/v2/users/orders/",methods=['GET'])
 def get_orders():
+    response =CustomerOrders.get_orders()
 
-    return jsonify({'orders':CustomerOrders.get_orders()}),200
+    return jsonify({'orders':response}),200
 
 
-@myapp.route("/users/orders/",methods=['POST'])
+@myapp.route("/api/v2/users/orders/",methods=['POST'])
 def make_order():
     order = request.json
     ordem = CustomerOrders(order.get('dish'),order.get('price'),order.get('status'),order.get('user_id'))
+    response = ordem.create_orders()
+    return jsonify({'order':response}), 201
 
-    return jsonify({'order':ordem.create_orders()}), 201
 
 
-
-@myapp.route("/users/orders/<int:id>",methods=['GET'])
+@myapp.route("/api/v2/users/orders/<int:id>",methods=['GET'])
 def get_order(id):
    
     order_no = id
-    return jsonify({'order details':CustomerOrders.get_order(order_no)}),200
+    response = CustomerOrders.get_order(order_no)
+    return jsonify({'order details':response}),200
     
 
 
-@myapp.route("/users/orders/<int:id>",methods=['PUT'])
+@myapp.route("/api/v2/users/orders/<int:id>",methods=['PUT'])
 def update_order_status(id):
     order = request.json
     ordem = CustomerOrders(order.get('dish'),order.get('price'),order.get('status'),order.get('user_id'))
     order_no = id
     status = order.get('status')
-    return jsonify({'order':ordem.update_order_status(status,order_no)}),201
+    response = ordem.update_order_status(status,order_no)
+    return jsonify({'order':response}),201
 
 
-@myapp.route("/menu",methods=['GET'])
+@myapp.route("/api/v2/menu",methods=['GET'])
 def get_menu():
-    pass
+    response = Menu.get_menu()
+    return jsonify({'Available_menu':response}),200
+    
 
-
-@myapp.route("/menu",methods=['POST'])
+@myapp.route("/api/v2/menu",methods=['POST'])
 def add_menu_item():
-    pass
+    menu = request.json
+    ordem = Menu(menu.get('dish'),menu.get('price'))
+    response = ordem.add_menu_items()
+
+    return jsonify({'Menu added':response}),201
+
+
     
